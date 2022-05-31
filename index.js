850342d7db8a43257f6f68b2e51b1350df7bd3f8
lena <lena.mashukova82@gmail.com>
2022-05-31 15:37:55 -0400

    PORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express') // the same as import express from 'express' (when babel installed)
 const bodyParser = require('body-parser')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 5000
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
