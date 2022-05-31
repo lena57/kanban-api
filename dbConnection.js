2227760bcfa1ab28b49ecbbcff2e061fead62e96
lena <lena.mashukova82@gmail.com>
2022-05-31 17:42:15 -0400

    edited dbConnection

--- dbConnection.js
+++ dbConnection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const connectionString = 'mongodb+srv://lena_mas:Uspech1!@cluster0.4nyxyyp.mongodb.net/?retryWrites=true&w=majority';
-const options = {};
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
 
 function dbConnection(){
     mongoose.connect(connectionString, options)
